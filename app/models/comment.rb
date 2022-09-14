45efcc3a51afac32e6b4e95b3406412a2319a597
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-09-14 09:38:38 +0700

    Add validation on model
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :body, presence: true, length: { maximum: 255, too_long: "Anda melebihi batas maksimal 255 karakter" }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates_associated :comments
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 255, too_long: "Anda melebihi batas maksimal 255 karakter" }
 end
