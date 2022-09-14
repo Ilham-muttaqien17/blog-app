09b87fcc996d1dc72f43276504d6bfd156fd7a5d
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-09-14 12:15:12 +0700

    Add respond formatter
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,14 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[ index show ]
 
   def index 
-    @posts = Post.all
+    @posts = Post.order("updated_at desc")
+
+    if !@posts.empty?
+      respond('index', data: @posts)
+    else
+      respond('index', data: nil, status_code: :not_found)
+    end
+
   end
 
   def show 
@@ -17,7 +24,10 @@ class PostsController < ApplicationController
 
     if @post.valid?
       redirect_to posts_path
+    else
+      redirect_to new_post_url, alert: "Gagal menambahkan post baru"
     end
+
   end
 
   def update 
@@ -42,12 +52,28 @@ class PostsController < ApplicationController
   def user_posts 
     user = User.find(params[:user_id])
     # binding.pry
-    @posts = user.posts.all
+    @posts = user.posts.all.order("updated_at desc")
+    
+    if !@posts.empty?
+      respond('user_posts', data: @posts)
+    else
+      respond('user_posts', data: nil, status_code: :not_found)
+    end
   end
 
   private
 
   def posts_params
     params.permit(:title, :body)
+  end
+
+  def respond(path, data: [], **args)
+    if args[:status_code]
+      status_code = args[:status_code]
+    end
+    respond_to do | format |
+      format.html { render path }
+      format.json { render json: { data: data }, status: status_code }
+    end
   end
 end
